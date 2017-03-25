9177fdf3118b91be8aecac60290909888d27ccfe
shooohei <kakei.shohei.1122@gmail.com>
2017-03-26 02:42:01 +0900

    [add] option: --dir [modify] header name: remote extension

--- combine.pl
+++ combine.pl
@@ -9,7 +9,9 @@ sub test()
 	my $indexFile = $_[0];
 	my $pathFile = $_[1];
 
-	$header .= basename($pathFile) . ",";
+	my $nameFile = basename($pathFile);
+	$nameFile =~ s/\.csv//;
+	$header .= $nameFile . ",";
 
 	open(my $fhFrom, '<', $pathFile) or die("error: $!");
 	my $i = 0;
@@ -28,12 +30,45 @@ sub test()
 	}
 }
 
+#-- entry point --#
+
 my $index = 0;
 my $numArg = $#ARGV + 1;
+my $optDir = 0;
+my $argDir = "";
+
 while ($index < $numArg)
 {
-	&test($index, $ARGV[$index]);
+	if ("--dir" eq $ARGV[$index])
+	{
+		$argDir = $ARGV[$index + 1];
+		$optDir = 1;
+		$index += 1;
+	}
+	else
+	{
+		# do nothing
+	}
 	$index++;
+}
+
+$index = 0;
+if (0 != $optDir)
+{
+	my @listFile = sort(glob($argDir . "/*.csv"));
+	foreach my $file (@listFile)
+	{
+		&test($index, $listFile[$index]);
+		$index++;
+	}
+}
+else
+{
+	while ($index < $numArg)
+	{
+		&test($index, $ARGV[$index]);
+		$index++;
+	}
 }
 
 print("$header\n");
